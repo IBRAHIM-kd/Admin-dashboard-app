744796441a97a970ac1041f46d5ef7367bb23c14
Your Name <you@example.com>
2018-06-14 11:27:47 +0200

    Setting up my deployment area and push to github already

---
+++ src/main/java/com/tangentsolution/org/EMPLOYEES/ADMIN/DASHBOARD/EmployeesAdminDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.tangentsolution.org.EMPLOYEES.ADMIN.DASHBOARD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAdminDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAdminDashboardApplication.class, args);
+	}
+}
